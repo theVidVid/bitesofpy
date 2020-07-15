65284c1b216b96451d33b4cc54aae88098ec3f88
Ian Vidaurre <Ianjvidaurre@gmail.com>
2020-07-15 00:04:34 -0700

    Solved bite10.firefox-developer.desktop

---
+++ bite10.py
@@ -0,0 +1,17 @@
+def divide_numbers(numerator, denominator):
+    """For this exercise you can assume numerator and denominator are of type
+         int/str/float.
+         Try to convert numerator and denominator to int types, if that raises a
+         ValueError reraise it. Following do the division and return the result.
+         However if denominator is 0 catch the corresponding exception Python
+         throws (cannot divide by 0), and return 0"""
+
+    try:
+        operation = int(numerator) / int(denominator)
+    except ZeroDivisionError:
+        return 0
+    return operation
+
+
+result = divide_numbers(55, 0)
+print(result)
